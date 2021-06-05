5036044aab9dbc3e6a025baeb87199ae81f9659f
Horácio Comé <horaciojr001@gmail.com>
2021-06-05 15:01:20 +0200

    remove unnecessary files

--- app/src/main/java/io/github/horaciocome1/firestore_flow/MainActivity.kt
+++ app/src/main/java/io/github/horaciocome1/firestore_flow/MainActivity.kt
@@ -20,38 +20,38 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    // common way
-    fun getPostsA() {
-        val db = FirebaseFirestore.getInstance()
-        val ref = db.collection("posts")
-        ref.addSnapshotListener { snapshot, error ->
-            if (error != null) {
-                // handle
-            } else if (snapshot != null) {
-                val posts = snapshot.toObjects(Post::class.java)
-                // set posts to UI
-            }
-        }
-    }
-
-    // with fireflow
-    @ExperimentalCoroutinesApi
-    suspend fun getPostsB() {
-        val db = FirebaseFirestore.getInstance()
-        db.collection("posts").getAsFlow<Post>().collect { posts ->
-            // set posts to UI
-        }
-    }
-
-    // with fireflow document
-    @ExperimentalCoroutinesApi
-    suspend fun getPost() {
-        val db = FirebaseFirestore.getInstance()
-        db.collection("posts")
-            .document("1")
-            .getAsFlow<Post>().collect { post ->
-                // set post to UI
-            }
-    }
+//    // common way
+//    fun getPostsA() {
+//        val db = FirebaseFirestore.getInstance()
+//        val ref = db.collection("posts")
+//        ref.addSnapshotListener { snapshot, error ->
+//            if (error != null) {
+//                // handle
+//            } else if (snapshot != null) {
+//                val posts = snapshot.toObjects(Post::class.java)
+//                // set posts to UI
+//            }
+//        }
+//    }
+//
+//    // with fireflow
+//    @ExperimentalCoroutinesApi
+//    suspend fun getPostsB() {
+//        val db = FirebaseFirestore.getInstance()
+//        db.collection("posts").getAsFlow<Post>().collect { posts ->
+//            // set posts to UI
+//        }
+//    }
+//
+//    // with fireflow document
+//    @ExperimentalCoroutinesApi
+//    suspend fun getPost() {
+//        val db = FirebaseFirestore.getInstance()
+//        db.collection("posts")
+//            .document("1")
+//            .getAsFlow<Post>().collect { post ->
+//                // set post to UI
+//            }
+//    }
 
 }
