beaf0b23fd2e5843e941a89ca2eee4ecca0a9c4c
Horácio Comé <hcome@pm.me>
2021-11-22 00:18:25 +0200

    adding support for specifying coroutine context to flow on

--- fireflow/src/main/java/io/github/horaciocome1/fireflow/MyFirestoreUtil.kt
+++ fireflow/src/main/java/io/github/horaciocome1/fireflow/MyFirestoreUtil.kt
@@ -1,116 +1,135 @@
 package io.github.horaciocome1.fireflow
 
-import com.google.firebase.firestore.*
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.EventListener
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.firestore.ktx.toObject
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.flowOn
+import kotlin.coroutines.CoroutineContext
 
 /**
  * returns a flow that listens to querySnapshot changes and offers them
  * @param query need to be specified
+ * @param coroutineContext default is Dispatchers.IO
  */
 @ExperimentalCoroutinesApi
 @Deprecated("Will be removed on next release", ReplaceWith("ref.asFlow()"))
-inline fun <reified T>getAsFlowFromQuery(query: Query): Flow<MutableList<T>> =
-    callbackFlow {
-        val listener = EventListener<QuerySnapshot> { snapshot, error ->
-            if (error != null) {
-                close(error)
-                return@EventListener
-            }
-            if (snapshot != null) {
-                val objets = snapshot.toObjects(T::class.java)
-                offer(objets)
-            }
+inline fun <reified T> getAsFlowFromQuery(
+    query: Query,
+    coroutineContext: CoroutineContext = Dispatchers.IO
+): Flow<MutableList<T>> = callbackFlow {
+    val listener = EventListener<QuerySnapshot> { snapshot, error ->
+        if (error != null) {
+            close(error)
+            return@EventListener
         }
-        val registration = query.addSnapshotListener(listener)
-        awaitClose { registration.remove() }
+        if (snapshot != null) {
+            val objets = snapshot.toObjects(T::class.java)
+            offer(objets)
+        }
     }
+    val registration = query.addSnapshotListener(listener)
+    awaitClose { registration.remove() }
+}.flowOn(coroutineContext)
 
 /**
  * returns a flow that listens to querySnapshot changes and offers them
+ * @param coroutineContext default is Dispatchers.IO
  */
 @ExperimentalCoroutinesApi
 @Deprecated("to remove", ReplaceWith("asFlow"))
-inline fun <reified T>Query.getAsFlow(): Flow<MutableList<T>> =
-    getAsFlowFromQuery(this)
+inline fun <reified T> Query.getAsFlow(
+    coroutineContext: CoroutineContext = Dispatchers.IO
+): Flow<MutableList<T>> = getAsFlowFromQuery(this, coroutineContext)
 
 /**
  * returns a flow that listens to querySnapshot changes and offers them
+ * @param coroutineContext default is Dispatchers.IO
  */
 @ExperimentalCoroutinesApi
-inline fun <reified T>Query.asFlow(): Flow<MutableList<T>> =
-    getAsFlowFromQuery(this)
+inline fun <reified T> Query.asFlow(
+    coroutineContext: CoroutineContext = Dispatchers.IO
+): Flow<MutableList<T>> = getAsFlowFromQuery(this, coroutineContext)
 
 /**
  * returns a flow that listens to querySnapshot changes and offer it
+ * @param coroutineContext default is Dispatchers.IO
  */
 @ExperimentalCoroutinesApi
-fun Query.snapshotAsFlow(): Flow<QuerySnapshot?> =
-    callbackFlow {
-        val listener = EventListener<QuerySnapshot> { snapshot, error ->
-            if (error != null) {
-                close(error)
-                return@EventListener
-            }
-            offer(snapshot)
+fun Query.snapshotAsFlow(
+    coroutineContext: CoroutineContext = Dispatchers.IO
+): Flow<QuerySnapshot?> = callbackFlow {
+    val listener = EventListener<QuerySnapshot> { snapshot, error ->
+        if (error != null) {
+            close(error)
+            return@EventListener
         }
-        val registration = addSnapshotListener(listener)
-        awaitClose { registration.remove() }
+        offer(snapshot)
     }
+    val registration = addSnapshotListener(listener)
+    awaitClose { registration.remove() }
+}.flowOn(coroutineContext)
 
 /**
  * returns a flow that listens to documentSnapshot changes and offers them
  * @param reference need to be specified
+ * @param coroutineContext default is Dispatchers.IO
  */
 @ExperimentalCoroutinesApi
 @Deprecated("Will be removed on next release", ReplaceWith("ref.asFlow()"))
-inline fun <reified T>getAsFlowFromReference(reference: DocumentReference): Flow<T?> =
-    callbackFlow {
-        val listener = EventListener<DocumentSnapshot> { snapshot, error ->
-            if (error != null) {
-                close(error)
-                return@EventListener
-            }
-            if (snapshot != null) {
-                val obj = snapshot.toObject<T>()
-                offer(obj)
-            }
+inline fun <reified T> getAsFlowFromReference(
+    reference: DocumentReference,
+    coroutineContext: CoroutineContext = Dispatchers.IO
+): Flow<T?> = callbackFlow {
+    val listener = EventListener<DocumentSnapshot> { snapshot, error ->
+        if (error != null) {
+            close(error)
+            return@EventListener
         }
-        val registration = reference.addSnapshotListener(listener)
-        awaitClose { registration.remove() }
+        offer(snapshot?.toObject<T>())
     }
+    val registration = reference.addSnapshotListener(listener)
+    awaitClose { registration.remove() }
+}.flowOn(coroutineContext)
 
 /**
  * returns a flow that listens to documentSnapshot changes and offers them
  */
 @Deprecated("to remove", ReplaceWith("asFlow"))
 @ExperimentalCoroutinesApi
-inline fun <reified T>DocumentReference.getAsFlow(): Flow<T?> =
-    getAsFlowFromReference(this)
+inline fun <reified T> DocumentReference.getAsFlow(
+    coroutineContext: CoroutineContext = Dispatchers.IO
+): Flow<T?> = getAsFlowFromReference(this, coroutineContext)
 
 /**
  * returns a flow that listens to documentSnapshot changes and offers them
  */
 @ExperimentalCoroutinesApi
-inline fun <reified T>DocumentReference.asFlow(): Flow<T?> =
-    getAsFlowFromReference(this)
+inline fun <reified T> DocumentReference.asFlow(
+    coroutineContext: CoroutineContext = Dispatchers.IO
+): Flow<T?> = getAsFlowFromReference(this, coroutineContext)
 
 /**
  * returns a flow that listens to documentSnapshot changes and offer it
  */
 @ExperimentalCoroutinesApi
-fun DocumentReference.snapshotAsFlow(): Flow<DocumentSnapshot?> =
-    callbackFlow {
-        val listener = EventListener<DocumentSnapshot> { snapshot, error ->
-            if (error != null) {
-                close(error)
-                return@EventListener
-            }
-            offer(snapshot)
+fun DocumentReference.snapshotAsFlow(
+    coroutineContext: CoroutineContext = Dispatchers.IO
+): Flow<DocumentSnapshot?> = callbackFlow {
+    val listener = EventListener<DocumentSnapshot> { snapshot, error ->
+        if (error != null) {
+            close(error)
+            return@EventListener
         }
-        val registration = addSnapshotListener(listener)
-        awaitClose { registration.remove() }
+        offer(snapshot)
     }
+    val registration = addSnapshotListener(listener)
+    awaitClose { registration.remove() }
+}.flowOn(coroutineContext)
